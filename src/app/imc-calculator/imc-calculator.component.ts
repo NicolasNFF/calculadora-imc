cb3728c77227fea88cdada5aa6fbd3d39f0b8907
Nicolas Ferrari <nicolassnfz.12@gmail.com>
2023-08-28 18:24:48 -0300

    Atualização no imc-component.ts

--- src/app/imc-calculator/imc-calculator.component.ts
+++ src/app/imc-calculator/imc-calculator.component.ts
@@ -20,22 +20,19 @@ export class ImcCalculatorComponent {
   }
 
   getClassification(imc: number): string {
-    if (imc < 16) {
-      return 'Magreza grave';
-    } else if (imc < 17) {
-      return 'Magreza moderada';
-    } else if (imc < 18.5) {
-      return 'Magreza leve';
-    } else if (imc < 25) {
-      return 'Saudável';
-    } else if (imc < 30) {
+    if (imc < 18.5) {
+      return 'Magreza';
+    } else if (imc >= 18.5 && imc < 24.9) {
+      return 'Peso normal';
+    } else if (imc >= 25 && imc < 29.9) {
       return 'Sobrepeso';
-    } else if (imc < 35) {
+    } else if (imc >= 30 && imc < 34.9) {
       return 'Obesidade Grau I';
-    } else if (imc < 40) {
-      return 'Obesidade Grau II (severa)';
+    } else if (imc >= 35 && imc < 40) {
+      return 'Obesidade Grau II';
     } else {
-      return 'Obesidade Grau III (mórbida)';
-    }
+      return 'Obesidade Grau III';
+  }
+
   }
 }
